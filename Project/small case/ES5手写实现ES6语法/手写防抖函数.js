277621f70cbd3ab3d93c1a5145d2784ceabf3c16
Dercyc <chengyue809@gmail.com>
2020-09-07 08:35:45 +0800

    modigy file format

---
+++ Project/small case/ES5手写实现ES6语法/手写防抖函数.js
@@ -0,0 +1,12 @@
+"use strict";
+function debounce(func, wait) {
+  let timeout = null;
+  return function () {
+    let context = this;
+    let args = arguments;
+    if (timeout) clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      func.apply(context, args);
+    }, wait);
+  };
+}
