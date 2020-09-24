873a5584c7218116cd660e416bec649f7cebb537
Dercyc <chengyue809@gmail.com>
2020-09-24 22:54:41 +0800

    reconstruction

--- Study_code/Typescript/test/hello.js
+++ Study_code/Typescript/test/hello.js
@@ -1,13 +1,12 @@
-"use strict";
 var Person = /** @class */ (function () {
-    function Person(hobby) {
-        this.hobby = hobby;
-    }
-    Person.prototype.echo = function () {
-        return '我喜欢：' + this.hobby;
-    };
-    return Person;
-}());
-var person = new Person('看书，习武，书法，旅游');
+  function Person(hobby) {
+    this.hobby = hobby;
+  }
+  Person.prototype.echo = function () {
+    return "我喜欢：" + this.hobby;
+  };
+  return Person;
+})();
+var person = new Person("看书，习武，书法，旅游");
 var hobby = person.echo();
 console.log(hobby);
