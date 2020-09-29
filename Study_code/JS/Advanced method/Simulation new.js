25ba6c4f4e91b3f4886e81d63229144ad87500e8
Dercyc <chengyue809@gmail.com>
2020-09-29 14:13:46 +0800

    add comment

--- Study_code/JS/Advanced method/Simulation new.js
+++ Study_code/JS/Advanced method/Simulation new.js
@@ -1,8 +1,13 @@
+/* 
+  创建一个新对象，并继承其构造函数的prototype，这一步是为了继承构造函数原型上的属性和方法
+  执行构造函数，方法内的this被指定为该新实例，这一步是为了执行构造函数内的赋值操作
+  返回新实例（规范规定，如果构造方法返回了一个对象，那么返回该对象，否则返回第一步创建的新对象）
+*/
 // new是关键字,这里我们用函数来模拟,new Foo(args) <=> myNew(Foo, args)
 function myNew(foo, ...args) {
   // 创建新对象,并继承构造方法的prototype属性, 这一步是为了把obj挂原型链上, 相当于obj.__proto__ = Foo.prototype
   let obj = Object.create(foo.prototype);
-
+  console.log(obj.__proto__);
   // 执行构造方法, 并为其绑定新this, 这一步是为了让构造方法能进行this.name = name之类的操作, args是构造方法的入参, 因为这里用myNew模拟, 所以入参从myNew传入
   let result = foo.apply(obj, args);
 
@@ -15,6 +20,15 @@ function myNew(foo, ...args) {
 function Foo(name) {
   this.name = name;
 }
-const newObj = myNew(Foo, "zhangsan");
+const newObj = myNew(Foo, "zs");
 console.log(newObj);
 console.log(newObj instanceof Foo);
+
+//class only ES6+
+class _Foo {
+  constructor(name) {
+    this.name = name;
+  }
+}
+const _newObj = new _Foo("ls");
+console.log(_newObj.name);
