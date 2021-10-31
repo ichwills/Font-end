8739491c4d06696564c41aee33be95a9991be2f6
Dercyc <chengyue809@gmail.com>
2021-10-31 23:28:37 +0800

    Reconstruct

--- Case/微信轰炸机.js
+++
@@ -1,9 +0,0 @@
-/**
- * 微信轰炸机,请勿滥用
- * 配合AutoJs app使用
- */
-setInterval(function () {
-  $(".edit_area").html("###"); //需要发送的内容
-  $(".edit_area").trigger($.Event("keydown", { keyCode: 13, ctrlKey: true }));
-  $(".btn_send").click();
-}, 3000);
